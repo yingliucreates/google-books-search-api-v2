9e5a3915326921b09bbf4d08f90dcc26ecd98847
yingliufilms <yingliufilms@gmail.com>
2023-05-03 17:33:12 -0400

    changed input box height + caret size

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,7 +1,7 @@
 const Form = () => {
 	return (
-		<form>
-			<input className="w-1/2 border-4 border-black focus:w-full" />
+		<form className="px-4 py-3">
+			<input className="w-1/2 h-16 p-1 text-4xl border-4 border-black  focus:w-full outline-none caret-w-2" />
 		</form>
 	);
 };
