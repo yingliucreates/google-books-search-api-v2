358406143684442e190c067a798f4c4a019ff507
yingliufilms <yingliufilms@gmail.com>
2023-05-21 18:18:34 -0400

    fixed clickout logic; added initial useFetch hook + mapData util

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -2,26 +2,19 @@ import { useEffect, useState } from "react";
 import Form from "./input";
 import Dropdown from "./dropdown";
 import Grid from "./grid";
-
-const items = [
-  "item1",
-  "item1",
-  "item1",
-  "item1",
-  "item1",
-  "item1",
-  "item1",
-  "item1",
-  "item1",
-  "item1",
-];
+import useFetch from "../lib/useFetch";
 
 const Container = () => {
   const [value, setValue] = useState("");
   const [displayDropdown, setDisplayDropdown] = useState(false);
   const [displayGrid, setDisplayGrid] = useState(false);
 
-  const onPassChange = (value) => setValue(value);
+  const { loading, list } = useFetch(value);
+
+  const onPassChange = (value, isClickedOut = false) => {
+    setValue(value);
+    if (isClickedOut) setDisplayDropdown(false);
+  };
   const onSubmit = () => {
     setDisplayDropdown(false);
     setDisplayGrid(true);
@@ -35,16 +28,20 @@ const Container = () => {
   return (
     <div>
       <div className="relative z-0">
-        <Form onPassChange={onPassChange} onSubmit={onSubmit} />
+        <Form
+          onPassChange={onPassChange}
+          onSubmit={onSubmit}
+          isLoading={loading}
+        />
         {displayDropdown ? (
           <div className="z-10 w-full absolute -bottom-18">
-            <Dropdown>{items}</Dropdown>
+            <Dropdown>{list}</Dropdown>
           </div>
         ) : null}
 
         {displayGrid ? (
           <div className="z-0 -bottom-18">
-            <Grid>{items}</Grid>
+            <Grid>{list}</Grid>
           </div>
         ) : null}
       </div>
